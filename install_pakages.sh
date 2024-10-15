b7595b21bac8cfe6cfdd23a5cdb106ac4f9d2a10
sherifatu <sherifarazak13@gmail.com>
2024-10-15 16:20:28 +0000

    task 1

---
+++ install_pakages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#this is to install cowsay
+sudo apt install cowsay
+cowsay "hello,world"
+
+#!/bin/bash
+#this is to install fortune
+sudo apt install fortune
+fortune
+
+#!/bin/bash
+#this is to install cmatrix
+sudo apt install cmatrix 
+cmatrix

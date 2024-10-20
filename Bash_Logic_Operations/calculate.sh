81fd59acfff465e16163cd04c78d5b1405a576a8
sherifatu <sherifarazak13@gmail.com>
2024-10-20 23:00:56 +0000

    task5

---
+++ Bash_Logic_Operations/calculate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#assign argument to variables
+#calculate the sum of the first two numbers
+#calculate the product with the third number
+
+Num1=4
+Num2=3
+Num3=6
+
+sum=$(( Num1 + Num2 ))
+product=$(( sum * Num3 ))
+echo "the sum of $Num1 and $Num2 is: $sum1" 
+echo "multiplying the sum by $Num3 gives: $product"
+

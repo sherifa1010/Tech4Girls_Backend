cdb9f074db179f5cfbdb81aeee2c9a4a3518be51
sherifatu <sherifarazak13@gmail.com>
2024-10-23 08:10:56 +0000

    task3

---
+++ string_methods.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#this is a comment
+
+my_string = "Hello,Achievers Ghana"
+print (my_string)
+
+print (my_string.lower())
+print (my_string.upper())
+
+print (my_string.replace ('Ghana','Girls'))
+
+words = ["Hello,Achievers Ghana"]
+joined_string = "".join(words)
+print("joined string:", joined_string)

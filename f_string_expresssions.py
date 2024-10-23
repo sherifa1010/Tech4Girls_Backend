4aa9d371dc48745886694c6458b2b8919114f6ec
sherifatu <sherifarazak13@gmail.com>
2024-10-23 08:22:46 +0000

    task4

---
+++ f_string_expresssions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#this is a comment
+
+a=10
+b=20
+sum = a + b
+difference = a -b
+product = a * b
+
+print(f"the sum of {a} and {b} is {sum}.")
+print(f"the difference of {a} and {b} is {difference}.")
+print(f"the product of {a} and {b} is {product}.")

0561b6491bdbede626fcaaec707f2fa9bd14d1ba
sherifatu <sherifarazak13@gmail.com>
2024-10-23 15:03:19 +0000

    task5

---
+++ string_indexing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#this is a comment
+#string_indexing.py
+
+sentence = "Python is powerful and easy to learn."
+print(len(sentence))
+print(sentence[0])
+print(sentence[-1])
+print(sentence[10:18])

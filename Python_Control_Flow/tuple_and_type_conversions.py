b8255bae73fb59e1437842b614df79d1afe09c6e
sherifatu <sherifarazak13@gmail.com>
2024-10-29 19:43:25 +0000

    assignment3

---
+++ Python_Control_Flow/tuple_and_type_conversions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+my_words = ("tomatoes","cat",400,"cabbage","melon")
+#first_element = my_words[0]
+#last_element = my_words[-1]
+#print("first_element:", first_element)
+#print("last_element:", last_element)
+
+#count = my_words.count('melon')
+#index = my_words.index(400)
+#print("count of 'melon':", count)
+#print("index of 400:", index)
+
+ 
+#value = 15
+#float_value = float(value)
+#print("integer to float", float_value)
+
+#number = 420
+#value = int(number)
+#print("float to integer:", value)
+
+#string = "30"
+#num_string = int(string)
+#print("string to integer:", string)
+
+string_list = ["Hello", "world" , "from", "sherry"]
+joined_string = "".join(string_list)
+print("Joined String:", joined_string)

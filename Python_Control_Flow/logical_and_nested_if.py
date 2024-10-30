523000f114ee19b1bcd9948389f23eeab023826e
sherifatu <sherifarazak13@gmail.com>
2024-10-30 15:07:06 +0000

    task1

--- Python_Control_Flow/logical_and_nested_if.py
+++ Python_Control_Flow/logical_and_nested_if.py
@@ -11,14 +11,16 @@
   #  print("you are employed but not astudent.")
 
 
-age = input("please enter your age")
-has_license = input("Do you have a driver's license? yes/no")
+age = int(input("please enter your age"))
 if age >= 18:
-    print("you are allowed to drive.")
-else
-     print("you need a drivers license to drive.")
-else
-     print("you are not old enough to drive.")
+   license = input("Do you have a driver's license? yes/no:")
+   if license == 'yes':
+       print("you are allowed to drive")
+   else:
+       print("You need a driver's license to drive.")
+else:
+    print("You are not old enough to drive.")
+
      
     
     

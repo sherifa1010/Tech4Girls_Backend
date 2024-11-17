1179cb9dcbd3f99c3b436007afb2a6cb9d204b7d
sherifatu <sherifarazak13@gmail.com>
2024-11-17 23:40:47 +0000

    task4

---
+++ OOP_Concepts/class_basics.py
@@ -0,0 +1,15 @@
+class car:
+    def __init__(self, make, model, year):
+        """initializing the instance variables."""
+        self.make = make
+        self.model = model
+        self.year = year
+        
+        def display_info(self):
+            """print car's details"""
+            print(f"car make: {self.make}")
+            print(f"car model: {self.model}")
+            print(f"car year: {self.year}")
+            
+my_car = car("benz" , "lexuxe", 2025)
+my_car.display_info()

f50420a510617c7deda924c37eeef5eb58605baf
sherifatu <sherifarazak13@gmail.com>
2024-11-18 15:16:47 +0000

    task3

--- OOP_Concepts/class_basics.py
+++ OOP_Concepts/class_basics.py
@@ -4,12 +4,10 @@ class car:
         self.make = make
         self.model = model
         self.year = year
-        
-        def display_info(self):
-            """print car's details"""
-            print(f"car make: {self.make}")
-            print(f"car model: {self.model}")
-            print(f"car year: {self.year}")
-            
+    def display_info(self):
+        """print car's details"""
+        print(f"car make: {self.make}")
+        print(f"car model: {self.model}")
+        print(f"car year: {self.year}") 
 my_car = car("benz" , "lexuxe", 2025)
 my_car.display_info()
